bc4dc83d387d112def4e3be4ead2c7532b529e03
Karmen <karmen.kukk@khk.ee>
2013-04-15 13:05:14 +0300

    yo recipes

--- index.php
+++ index.php
@@ -30,12 +30,6 @@
 <script src="assets/js/plugins.js"></script>
 <script src="assets/js/main.js"></script>
 
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-		g.src='//www.google-analytics.com/ga.js';
-		s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>
+
 </body>
 </html>

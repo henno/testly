7c206c189070c8cf5530d314931622e4985b43b7
Karmen <karmen.kukk@khk.ee>
2013-04-17 11:50:48 +0300

    Add tests controller. Change eafult controller to tests. Define base_url parameter.

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 define('DATABASE_HOSTNAME', 'localhost');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_DATABASE', 'testly');
-define('DEFAULT_CONTROLLER', 'Welcome');
+define('DEFAULT_CONTROLLER', 'tests');
 define('BASE_URL', '/testly/');
---
+++ controllers/tests.php
@@ -0,0 +1,8 @@
+<?php
+
+class tests {
+	public $requires_auth = true;
+	function index(){
+
+	}
+}

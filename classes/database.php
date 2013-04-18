15307ef40d17d691d7db7d4ebddc78aa9099a2cc
Karmen <karmen.kukk@khk.ee>
2013-04-18 13:25:51 +0300

    Comment index.php, database.php, user.php.

--- classes/database.php
+++ classes/database.php
@@ -1,23 +1,20 @@
 <?php
 
-mysql_connect(DATABASE_HOSTNAME, DATABASE_USERNAME) or mysql_error();
-mysql_select_db(DATABASE_DATABASE) or mysql_error();
-mysql_query("SET NAMES 'utf8'");
+mysql_connect(DATABASE_HOSTNAME, DATABASE_USERNAME) or mysql_error(); // loob ühenduse mysql serveriga
+mysql_select_db(DATABASE_DATABASE) or mysql_error(); // ühendus andmebaasiga
+mysql_query("SET NAMES 'utf8'"); // päringud mis saadab on utf8 kodeeringus, et server saaks aru
 mysql_query("SET CHARACTER 'utf8'");
 
-function q($sql, $debug = false){
-	if ($debug){
-		print "<pre>$sql</pre>";
-	}
-}
+
 function get_one($sql, $debug = false){
-	if ($debug){
+	if ($debug){ // kui debug on TRUE
 		print "<pre>$sql</pre>";
 	}
 	$q = mysql_query($sql) or exit(mysql_error());
 	if (mysql_num_rows($q) === false){
 		exit($sql);
 	}
-	$result = mysql_fetch_row($q);
+	$result = mysql_fetch_row($q); // teeb massiivi $q-st
+	// kas $result on array ja on rohkem kui 0 elementi, siis tagastab esimese elemendi
 	return is_array($result) && count($result) > 0 ? $result[0] : null;
 }
--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
 
-require 'config.php';
+require 'config.php'; //paneb käima/võtab kasutusele config-i
 require 'classes/Request.php';
 require 'classes/user.php';
 require 'classes/database.php';
 
 if (file_exists('controllers/'.$request->controller.'.php')){
-	require 'controllers/'.$request->controller.'.php';
+	require 'controllers/'.$request->controller.'.php'; // kui olemas, võta kasutusele
 	$controller = new $request->controller;
 		if (isset($controller->requires_auth)){
 			$_user->require_auth();
 		}
-		$controller->{$request->action}();
+		$controller->{$request->action}(); // sulud sest action on index() auth.php-s
 } else {
-	//var_dump($request->controller);
 	echo "The page '{$request->controller}' does not exist";
 }

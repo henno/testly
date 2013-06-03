99ada4193b1da276b22e5c08e9b161c852e28358
Karmen <karmen.kukk@khk.ee>
2013-06-03 11:39:18 +0300

    [fixes #50967371] Create Osalejad tab and table in it.

--- views/tests_edit_view.php
+++ views/tests_edit_view.php
@@ -11,7 +11,7 @@
 	<ul>
 		<li><a href="#tabs-1">Üldine</a></li>
 		<li><a href="#tabs-2">Küsimused</a></li>
-		<li><a href="#tabs-3">Aenean lacinia</a></li>
+		<li><a href="#tabs-3">Osalejad</a></li>
 	</ul>
 	<div id="tabs-1">
 		<form method="post" id="test_edit_form">
@@ -63,7 +63,25 @@
 		</div>
 	</div>
 	<div id="tabs-3">
-		<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-		<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+		<table id="participants-table" class="table table-bordered table-striped" style="width: auto">
+			<thead>
+			<tr>
+				<th><input style="margin: 5px" type="checkbox">Vali kõik</th>
+				<th>Isik</th>
+				<th>Grupp</th>
+				<th>Ajavahemik</th>
+				<th>Punktid</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td><input style="margin: 5px" type="checkbox"></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			</tbody>
+		</table>
 	</div>
 </div>

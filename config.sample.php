25c5efaeee3dd131b524fd923cc883b5930cf3be
Henno Täht <henno.taht@khk.ee>
2013-06-03 12:44:14 +0300

    Support different DB configurations (add config.php to gitignore, create config.sample.inc as an example)

---
+++ config.sample.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_DATABASE', 'testly');
+define('DEFAULT_CONTROLLER', 'tests');
+define('BASE_URL', '/testly/');
+define('ASSETS_URL', '/testly/assets/');

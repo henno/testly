86752e5d7c55912015cbab17953854b505bff539
Karmen <karmen.kukk@khk.ee>
2013-06-04 22:04:00 +0300

    [fixes #50970089] Change addMultipleResponse function.

--- assets/js/tests_add_edit.js
+++ assets/js/tests_add_edit.js
@@ -1,5 +1,6 @@
-var current_type_id = 2;
+var current_type_id = 1;
 var ylakoma = "'";
+var jutum2rk = '"';
 
 function addMultipleChoice() {
 	var html = '<div class="answer-option"><input type="radio" name="mc.correct" value="<id>">&nbsp;<textarea name="mc.answer.<id>"></textarea></div>';
@@ -8,11 +9,14 @@ function addMultipleChoice() {
 	$('#multiple-choice-options').append(html);
 	return false;
 }
+var i = 0;
+var id = 'tekstikast_0';
 function addMultipleResponse() {
-	var html = '<div class="answer-option"><input type="checkbox" name="mr.correct" value="<id>">&nbsp;<textarea name="mr.answer.<id>"></textarea></div>';
-	var textareas_count = $('#multiple-response-answer-option textarea').length;
-	html = html.replace(/<id>/g, textareas_count + 1);
-	$('#multiple-response-answer-option').append(html);
+	if ($('#' + id).val().length > 0) {
+		i = i+1;
+		id = 'tekstikast_'+i;
+		$('#answer_options').append('<div><input type="checkbox" style="margin: 5px; margin-bottom: 10px"><textarea oninput="addMultipleResponse()" id="tekstikast_'+i+'"></textarea></div>');
+	}
 	return false;
 }
 function removeMultipleChoice() {
@@ -31,7 +35,7 @@ function checkForm() {
 	var elements = $('#question_type_id_' + current_type_id + 'input[type=checkbox]:not(.shuffle_answers), #question_type_id_' + current_type_id + 'input[type=radio]:not(#shuffle)');
 	var textbox = $('#question_type_id_' + current_type_id + 'textarea');
 	for (var i = 0; i < elements.length; i++) {
-		if ($(elements[i]).attr('checked') && $.trim($(textboxe[i]).val()) != "") {
+		if ($(elements[i]).attr('checked') && $.trim($(textbox[i]).val()) != "") {
 			return true;
 		}
 	}
@@ -70,18 +74,25 @@ $(function () {
 		// change answer_option type
 		switch ($(this).val().trim()) {
 			case '1':
-				// true/false
+				// õige/vale
 				$('#answer_options div').remove();
+				$('#answer_options').append('<p>Märgi ära õige vastus:</p>');
 				$('#answer_options').append('<div style="padding: 5px"><input style="margin: 5px; margin-bottom: 9px" type="radio" id="answer_option[0]" name="tfselected">Jah</div>');
 				$('#answer_options').append('<div style="padding: 5px"><input style="margin: 5px; margin-bottom: 9px" type="radio" id="answer_option[1]" name="tfselected">Ei</div>');
 				break;
 			case '2':
+				// mitmikvalik
 				$('#answer_options div').remove();
 				break;
 			case '3':
+				// mitmikvastus
 				$('#answer_options div').remove();
+				$('#answer_options').append('<p>Märgi ära õiged vastused:</p>');
+				$('#answer_options').append('<div><input class="input" type="checkbox" style="margin: 5px; margin-bottom: 10px"><textarea oninput="addMultipleResponse()" id="tekstikast_0"></textarea></div>');
+
 				break;
 			case '4':
+				// fill in the blank
 				$('#answer_options div').remove();
 				break;
 			default:

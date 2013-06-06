6ba61c40ca329367a17dbc34aba0f76c54ea5d21
kaia <kaia.konsap@khk.ee>
2013-06-06 12:21:20 +0300

    [# 51021149] Make javascript for index view to add groups, half ready

--- views/groups_index_view.php
+++ views/groups_index_view.php
@@ -6,8 +6,8 @@
 	</style>
 <button class=" btn btn-primary" type="button">Kustuta valitud</button>
 <div id="right" style="float: right; width: 400px;display: inline-block">
-<form method="POST" style="margin: 0;padding: 0"><input type="text" style="float: left"></form>
-<button class="btn  btn-primary" type="submit" style="float: left;">Lisa grupp</button>
+<form id="add_group" method="POST" style="margin: 0;padding: 0"><input type="text" id="add" style="float: left"></form>
+<button class="btn  btn-primary" style="float: left;" onclick="add_group()">Lisa grupp</button>
 </div>
 	<table id="groups-table" class="table table-bordered table-striped" style="margin-top: 50px">
 		<thead>
@@ -18,11 +18,19 @@
 		</tr>
 		</thead>
 		<tbody>
-		<?if(!empty($groups)):foreach( $groups as $group):?>
-		<tr>
-				<td><input type="checkbox" style="margin-top: -2px"></td>
-				<td style="padding-left: 20px;padding-right: 50px"> <a href="<?BASE_URL?>groups/selected/<?$group["group_id"]?>"><?=$group["group_name"]?></a> </td>
-			<td style="padding-left: 20px;padding-right: 50px"> <?if(!empty($number)): echo $number[0]["number"];endif;?></td>
+
+		<tr id="new_groups">
+
+		</tr>
+		<?if(isset($groups)):foreach( $groups as $group):?>
+
+		<tr id="my_groups">
+			<td id="checkbox1"><input type="checkbox"></td>
+				<td id="group_name1" style="padding-left: 20px;padding-right: 50px">
+					<a href="<?BASE_URL?>selected/<?if(isset($group_id)): echo $group_id;
+					endif?>"><?=$group["group_name"]?></a> </td>
+			<td style="padding-left: 20px;padding-right: 50px"> <?if(!empty($group["number"])): echo $group["number"][0];endif;
+				?></td>
 		</tr>
 		<?endforeach;endif?>
 		</tbody>

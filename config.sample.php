111819fe313bc9a730e46d064d4d2afa166ca5e5
Henno Täht <henno.taht@khk.ee>
2013-06-10 20:12:27 +0300

    Remove password from config.php

--- config.sample.php
+++ config.sample.php
@@ -2,7 +2,7 @@
 
 define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
 define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', 'toor');
+define('DATABASE_PASSWORD', '');
 define('DATABASE_DATABASE', 'testly');
 define('DEFAULT_CONTROLLER', 'tests');
 define('BASE_URL', '/testly/');
